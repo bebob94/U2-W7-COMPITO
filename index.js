201ee0042bee5463969186720055e231195079c4
Alberto Macis <bebo.macis@gmail.com>
2023-01-27 21:49:09 +0100

    add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const contenitore1 = document.getElementById("contenitore1");
 const contenitore2 = document.getElementById("contenitore2");
-const contenitore3 = document.getElementById("contenitore3");
+
+const APIUrl = "https://striveschool-api.herokuapp.com/api/deezer/search?q=";
 
 const fetchByQuery = async (query) => {
   const res = await fetch(`${APIUrl}${query}`);
@@ -20,14 +21,15 @@ const canzoni = async () => {
       let songs = canzoni[i];
       contenitore1.innerHTML += `
             <div class="col  m-1">
-                <div class="card text-bg-success text-dark " style="width: 18rem;" height="150px">
-                    <img src="${songs.album.cover_medium}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${songs.title}</h5>
-                    <p class="card-text">Artist:  ${songs.artist.name}</p>
-                    <a href="${songs.link}" class="btn btn-primary">deezer</a>
-                </div>
-                </div>
+                <a href="${songs.link}">
+                    <div class="card text-bg-dark text-light " style="width: 18rem;" height="150px">
+                        <img src="${songs.album.cover_medium}" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${songs.title}</h5><hr />
+                            <p class="card-text">${songs.artist.name}</p>
+                        </div>
+                    </div>
+                </a>
             </div> `;
     }
   } catch (error) {
@@ -44,14 +46,21 @@ const canzoni = async () => {
     for (let i = 0; i < 1; i++) {
       let songs = canzoni[i];
       contenitore2.innerHTML = `
-    <div class="col col-6  m-1">
-        <div class="card  mb-3 m-3">
-            <img src="${songs.album.cover_big}" class="card-img-top w-100" height="400px" alt="...">
-        <div class="card-body ">
-            <h5 class="card-title">${songs.title}</h5>
-            <p class="card-text">Artist: ${songs.artist.name}</p>
-            <a href="${songs.link}" class="btn btn-primary">deezer</a>
-        </div>
+    <div class="col">
+        <a href="${songs.link}">
+            <div class="card text-bg-dark text-light mb-3" ">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="${songs.album.cover_big}" class="img-fluid rounded-start h-100" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body position-absolute bottom-0 ">
+                            <h5 class="card-title mb-3">${songs.title}</h5> <hr />
+                            <p class="card-text mb-2">${songs.artist.name}</p>
+                        </div>
+                    </div>
+            </div>
+        </a>
     </div>`;
     }
   } catch (error) {
@@ -62,21 +71,21 @@ const canzoni = async () => {
 
 async function favouriteAlbum() {
   try {
-    const arrayDiQuery = ["nonostante tutto", "eclissi gemitaiz", "ali iltre"];
+    const arrayDiQuery = ["Playlist salmo", "prisoner 709", "caos fibra"];
     for (let i = 0; i < arrayDiQuery.length; i++) {
       const singolaQuery = arrayDiQuery[i];
       let [canzone] = await fetchByQuery(singolaQuery);
       const sezione = document.querySelector(".carousel-inner");
       sezione.innerHTML += ` 
-        <div class="carousel-item ${i == 0 ? "active" : " "}">
+        <div class="carousel-item ${i == 0 ? "active" : " "} rounded-4">
         <img src="${
           canzone.album.cover_medium
         }" class="d-block w-100" alt="..." />
-          </div>;`;
+          </div>`;
     }
   } catch (error) {
     console.log(error);
   }
 }
-favouriteAlbum();
 canzoni();
+favouriteAlbum();
